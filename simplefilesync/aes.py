63ae18ac4df38e7d5de23380841079714aae67a8
diademiemi <emilia@diademiemi.me>
2022-04-25 23:23:49 +0200

    Use pickle instead of JSON

--- simplefilesync/aes.py
+++ simplefilesync/aes.py
@@ -4,7 +4,7 @@ import simplefilesync.config as config
 
 def encrypt_message(message):
     cipher = AES.new(config.config['shared_secret'].encode(), AES.MODE_EAX)
-    ciphertext, tag = cipher.encrypt_and_digest(message.encode())
+    ciphertext, tag = cipher.encrypt_and_digest(message)
     return cipher.nonce, tag, ciphertext
 
 def decrypt_message(nonce, tag, ciphertext):
--- simplefilesync/socket.py
+++ simplefilesync/socket.py
@@ -4,8 +4,8 @@ import simplefilesync.aes as aes
 import simplefilesync.config as config
 import simplefilesync.filesystem as filesystem
 
+import pickle
 from struct import unpack, pack
-import json
 
 def start():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,27 +25,35 @@ def start():
 
             finally:
                 connection.close()
-            
-            json_content = aes.decrypt_message(rec_content[:16], rec_content[16:32], rec_content[32:])
-            file = json.loads(json_content.decode(), strict=False)
+            # try:
+            pickle_content = aes.decrypt_message(rec_content[:16], rec_content[16:32], rec_content[32:])
+            file = pickle.loads(pickle_content)
+            print(file)
             print("Received new {} from {}".format(file['filename'], addr[0]))
             filesystem.write_file(file['filename'], file['content'])
+            # except Exception as e:
+            #     print(e)
 
 def sendAll(file):
     for host in config.config['remote_hosts']:
         send(host, file)
 
 def send(host, file):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, config.config['port']))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, config.config['port']))
 
-    # Send file
-    with open(file, 'r') as f:
-        json_content = '{ "filename": "%s", "content": "%s" }'%(file, f.read())
-        nonce, tag, ciphertext = aes.encrypt_message(json_content)
-        length = pack('>Q', len(nonce + tag + ciphertext))
-        s.sendall(length)
-        s.sendall(nonce + tag + ciphertext)
-        print("Sent new {} to {}".format(file, host))
-    
-    s.close()
+        # Send file
+        with open(file, 'r') as f:
+            
+            pickle_content = pickle.dumps({ "filename": file, "content": f.read() })
+            print(pickle_content)
+            nonce, tag, ciphertext = aes.encrypt_message(pickle_content)
+            length = pack('>Q', len(nonce + tag + ciphertext))
+            s.sendall(length)
+            s.sendall(nonce + tag + ciphertext)
+            print("Sent new {} to {}".format(file, host))
+        
+        s.close()
+    except Exception as e:
+        print(e)
